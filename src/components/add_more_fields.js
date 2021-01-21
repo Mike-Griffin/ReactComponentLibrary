7ee5dcc5f9f3b5e7c42db6ad893d66b6ed1bde24
mike-griffin <mike_griffin@comcast.net>
2021-01-20 19:31:04 -0800

    Updated the field to map through state objects

--- src/components/add_more_fields.js
+++ src/components/add_more_fields.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
+import { Select } from '@material-ui/core'
 
 class AddMoreFields extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            recipeLines : [
+                {'quantity': '10'},
+                {'quantity': '1903'}
+            ]
+        }
+    }
+
     render()  {
         return (
             <div>
                 Add More Fields
                 <div>
                     <form>
-                        <TextField
-                            name="quantity"
-                            label="Quantity"
-                        />
+                        {this.state.recipeLines.map(recipeLine => (
+                            <div>
+                                <TextField
+                                    name="quantity"
+                                    label="Quantity"
+                                    value={recipeLine.quantity}
+                                />
+                                <Select
+                                />
+                            </div>
+                        ))}
+
                     </form>
                 </div>
 
